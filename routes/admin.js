f2b9f1dcd50098ebadb57ee4834ca684e891db99
Farhan <farhankasmani@live.com>
2020-07-11 18:46:08 +1000

    serving html page with __dirname

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
-const express = require ("express");
+const path = require('path');
+
+const express = require("express");
 
 const router = express.Router();
 // implicitly /admin is prefixed to both the routes
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    `<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>`
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,13 @@
-const express = require ("express");
+const path = require("path");
+
+const express = require("express");
 
 const router = express.Router();
 
 // .get will do exact match but .use will handle any incoming method
 router.get("/", (req, res, next) => {
-  res.send("<h1>Hello from express</h1>");
+  // __dirname is a variable provided by node, it hold the absolute path this file - always use it instead of absolute path
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
