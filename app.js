43c1f30aed6784056986691c44434e293be45315
Farhan <farhankasmani@live.com>
2020-07-11 17:51:41 +1000

    parsing incoming req using body-parser

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
 // middleware
 
-app.use("/", (req, res, next) => {
-  console.log("this always runs");
-  next(); // Allows to execute next middleware
-});
+app.use(bodyParser.urlencoded()); // parse body of the response (it will not parse files)
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In the next middleware");
-  res.send(`<h1>The "Add product" Page </h1>`);
+  res.send(
+    `<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>`
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In the next middleware");
   res.send("<h1>Hello from express</h1>");
 });
 
