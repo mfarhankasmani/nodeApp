fd6db308df04166db37013a3df214f5ae05e63fc
Farhan <farhankasmani@live.com>
2020-07-11 17:08:37 +1000

    adding middlewares

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
+//below func is middleware
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next();
+});
 
+//without next function in above middleware, below middleware will not be called
+app.use((req, res, next) => {
+  console.log("In the next middleware");
+});
+
+const server = http.createServer(app);
 server.listen(3000);
