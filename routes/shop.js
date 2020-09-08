b21a75def96be8a1be548d1a39d42aea74593879
Farhan <farhankasmani@live.com>
2020-09-08 21:16:35 +1000

    route protection

--- routes/shop.js
+++ routes/shop.js
@@ -4,6 +4,9 @@ const express = require("express");
 
 const shopController = require("../controllers/shop");
 
+//using middleware for auth check
+const isAuth = require("../middleware/is-auth");
+
 const router = express.Router();
 
 router.get("/", shopController.getIndex);
@@ -15,13 +18,13 @@ router.get("/products", shopController.getProducts);
 
 router.get("/products/:productId", shopController.getProduct);
 
-router.get("/cart", shopController.getCart);
-router.post("/cart", shopController.postCart);
+router.get("/cart", isAuth, shopController.getCart);
+router.post("/cart", isAuth, shopController.postCart);
 
-router.post("/cart-delete-item", shopController.postCardDeleteProduct);
+router.post("/cart-delete-item", isAuth, shopController.postCardDeleteProduct);
 
-router.post("/create-order", shopController.postOrders);
-router.get("/orders", shopController.getOrders);
+router.post("/create-order", isAuth, shopController.postOrders);
+router.get("/orders", isAuth, shopController.getOrders);
 
 // router.get("/checkout", shopController.getCheckout);
 
