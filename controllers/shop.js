a275f92b0554e80a054502476b9473fa268b3dfd
Farhan <farhankasmani@live.com>
2020-09-18 14:59:26 +1000

    Downloading files with authentication

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const { Product } = require("../models/product");
 const Order = require("../models/order");
 
@@ -149,5 +152,18 @@ exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     path: "/checkout",
     pageTitle: "Checkout",
+  });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName = `invoice-${orderId}.pdf`;
+  const invoicePath = path.join("data", "invoices", invoiceName);
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      console.log(err);
+      return next(err);
+    }
+    res.send(data);
   });
 };
