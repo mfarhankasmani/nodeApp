92fb5668f19e40946bc96589ad0894cc93b3a73f
Farhan <farhankasmani@live.com>
2020-09-18 21:43:35 +1000

    DOM manipulation using JS

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,6 +3,8 @@ const deleteProduct = (btn) => {
   const prodId = parentNode.querySelector("[name=productId]").value;
   const csrf = parentNode.querySelector("[name=_csrf]").value;
 
+  const productElement = btn.closest("article");
+
   fetch(`/admin/products/${prodId}`, {
     method: "DELETE",
     headers: {
@@ -14,6 +16,8 @@ const deleteProduct = (btn) => {
     })
     .then((data) => {
       console.log({ data });
+      //manupulating DOM
+      productElement.parentNode.removeChild(productElement);
     })
     .catch((err) => console.log({ err }));
 };
